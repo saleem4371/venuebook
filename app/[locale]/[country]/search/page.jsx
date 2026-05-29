217126686f42fd52a7ced3abc102c9009669951f
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-05-29 18:43:53 +0530

    Delete app/[locale]/[country]/search/page.jsx

--- app/[locale]/[country]/search/page.jsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Page({ params }) {
-  const country = params?.country || "in";
-  const locale = params?.locale || "en";
-
-  redirect(`/${country}/${locale}/search/venues`);
-}
