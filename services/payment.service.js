436d84103efc276bf2fd5f6df54c6aebe640bf57
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-07-03 22:49:52 +0530

    Add files via upload

--- services/payment.service.js
+++ services/payment.service.js
@@ -10,10 +10,21 @@ export const verify_subscription = (id) => {
   return api.get(`/cashfree/verify_subscription/${id}`);
 };
 
+
+
 export const cashfree_plans = (id, category) => {
   return api.get(`/cashfree/cashfree_plans/${id}`, {
     params: {
       category,
     },
   });
+};
+
+/* Stripe subscription */
+export const stripe_subscription = (payload) => {
+  return api.post(`/stripe/subscription`,payload);
+};
+
+export const verify_stripe_subscription = (id) => {
+  return api.get(`/stripe/verify_subscription/${id}`);
 };
