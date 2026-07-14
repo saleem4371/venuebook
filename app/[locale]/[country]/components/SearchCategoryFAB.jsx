8daaafe2ce49b7af7f9a0822b3e947b61fa18b92
Shawn-21 <shawnlanishdsouza@gmail.com>
2026-07-14 12:30:10 +0530

    Delete app/[locale]/[country]/components directory

--- app/[locale]/[country]/components/SearchCategoryFAB.jsx
+++
@@ -1,3 +0,0 @@
-// This file is intentionally empty.
-// The universal FAB is now handled inside CategoryNavigator with fabBreakpoint prop.
-// See: app/[locale]/[country]/home/components/CategoryNavigator.jsx
