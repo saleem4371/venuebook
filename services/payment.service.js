2f0ae5306a219c837b348fc50a768beb435ca91d
saleem4371 <32508556+saleem4371@users.noreply.github.com>
2026-08-11 13:13:01 +0530

    Update payment.service.js

--- services/payment.service.js
+++ services/payment.service.js
@@ -60,84 +60,26 @@ export const onlinepayment = (payload) => {
 export const cancelBooking = (payload) => {
   return api.post(`/razorpay/cancelBooking`,payload);
 };
-export const editBookingRequest = (payload) => {
-  return api.post(`/booking/editBookingRequest`,payload);
+
+
+export const razorpay_subscription = (payload) => {
+  return api.post(`/razorpay/razorpay_subscription`,payload);
 };
 
-import api from "@/lib/axios";
-
-/* Cashfree subscription */
-export const cashfree_subscription = (payload) => {
-  return api.post(`/cashfree/subscription`,payload);
-};
-
-/* Verify subscription */
-export const verify_subscription = (id) => {
-  return api.get(`/cashfree/verify_subscription/${id}`);
-};
-
-
-
-export const cashfree_plans = (id, category) => {
-  return api.get(`/cashfree/cashfree_plans/${id}`, {
-    params: {
-      category,
-    },
-  });
-};
-
-/* Stripe subscription */
-export const stripe_subscription = (payload) => {
-  return api.post(`/stripe/subscription`,payload);
-};
-
-export const verify_stripe_subscription = (id) => {
-  return api.get(`/stripe/verify_subscription/${id}`);
-};
-
-//Razor Pay 
-
-// export const createOrder = (payload) => {
-//   return api.post("/razorpay/create-order", payload);
+// export const verify_razorpay_subscription = (payload) => {
+//   return api.post(`/razorpay/subscription/verify`,payload);
 // };
 
-// export const verifyPayment = (payload) => {
-//   return api.post("/razorpay/verify", payload);
-// };
-
-export const createOrder = async (payload) => {
-  const { data } = await api.post("/razorpay/create-order", payload);
-  return data;
-};
-
-export const verifyPayment = async (payload) => {
-  const { data } = await api.post("/razorpay/verify", payload);
-  return data;
-};
-
-
-export const createOnlineBooking = (payload) => {
-  return api.post(`/razorpay/createOnlineBooking`,payload);
-};
-
-export const onlinepayment = (payload) => {
-  return api.post(`/razorpay/onlinepayment`,payload);
-};
-export const cancelBooking = (payload) => {
-  return api.post(`/razorpay/cancelBooking`,payload);
-};
-export const editBookingRequest = (payload) => {
-  return api.post(`/booking/editBookingRequest`,payload);
-};
-
-export const verify_razorpay_subscription = (payload) => {
+export const verifyRazorpaySubscription = (payload) => {
   return api.post(`/razorpay/subscription/verify`,payload);
 };
 
+export const verify_razorpay_subscription = (id) => {
+  return api.get(`/razorpay/verify_subscription/${id}`);
+};
 
+export const editBookingRequest = (payload) => {
+  return api.post(`/booking/editBookingRequest`,payload);
+};
 
-
-
-
-
-
+ 
